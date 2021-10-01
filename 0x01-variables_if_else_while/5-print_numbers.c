2cb40ac65ba505c34472874e4db0df08bd73a654
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-01 15:40:19 +0000

    Added files task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Prints all single digit numbers of base 10 starting from 0
+ */
+
+int main(void)
+{
+	int num = 0;
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
