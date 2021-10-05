3842258c5367d33960163eef096a487ca0cdc23a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-05 14:57:46 +0000

    Added file task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+int _islower(int c);
+
+/**
+ * _islower - Checks for a lowercase character
+ *
+ * c - input character given to check if it's lowercase
+ *
+ * Return - lower is 1 if c is lowercase or 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int lower;
+
+	if (c >= 65 || c <= 90)
+	{
+		lower = 0;
+	}
+	else
+	{
+		lower = 1;
+	}
+	return (lower);
+}
