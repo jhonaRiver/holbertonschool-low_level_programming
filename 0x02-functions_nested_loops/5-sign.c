f352f6a347aaf09ea3eccd64d311060a6d73f8ab
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-05 15:32:22 +0000

    Added file task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+int print_sign(int n);
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * @n: input character to be checked
+ *
+ * Return: 1 if n > 0, 0 if n = 0 or -1 if n < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
