3df43e4c96616f20c009e6afdd05bc5c25102e13
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-05 14:11:11 +0000

    Modified file task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return - Always 0 (Success)
- */
-
 void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 
 /**
  * print_alphabet - Prints the alphabet in lowercase
