b586a9dc6d17942d5a7426a6a120bcf7313263b3
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-08 14:47:24 +0000

    Modified file task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
  * print_most_numbers - Prints the numbers from 0 to 9 except 2 and 4
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
@@ -12,7 +12,7 @@ void print_most_numbers(void);
 	{
 		if (i != '2' && i != '4')
 		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
