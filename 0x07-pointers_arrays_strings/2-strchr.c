d3607548808beabccc37f48b395b29a870982428
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-10-18 14:48:55 +0000

    Added file task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String to search in
+ * @c: Character to look for
+ * Return: Pointer to position of c or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (c == *s)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
