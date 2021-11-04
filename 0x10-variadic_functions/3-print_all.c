958de19ac831741ba67cd29f41dfae8f7eb065d6
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-04 16:29:37 +0000

    Modified file task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -54,7 +54,7 @@ void print_a_char(char *separator, va_list args)
 
 void print_a_integer(char *separator, va_list args)
 {
-	printf("%s%i", separator, va_arg(args, init));
+	printf("%s%i", separator, va_arg(args, int));
 }
 
 /**
