40cc16ed03a87d3e5dd0c655dd6c68fef06f22e9
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-12 14:53:08 +0000

    Added README and header files:

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list - singly linked list
+ * @str: node with the data stored
+ * @len: length of str
+ * @next: node pointing to the next node
+ */
+typedef struct list
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+#endif
