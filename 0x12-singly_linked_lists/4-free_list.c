a3f6687125ca9e3c7afb92dcd8da712c9f1ee4e1
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-12 17:22:04 +0000

    Added file task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: first node of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head`);
+}
