0163fcc67d72314323d85623ae3236b78e3517b9
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-16 14:51:25 +0000

    Modified file task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,18 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = (*head);
-		*head = (*head)->next;
-		free(temp);
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
 	}
 	free(*head);
 	head = 0;
