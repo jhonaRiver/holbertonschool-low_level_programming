fa9faa8af25129e093e74ac848ac9d3bd7aaf472
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2021-11-16 17:06:46 +0000

    Modified file task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count;
 
 	temp = *head;
-	for (count = 1; temp != NULL && count != index)
+	for (count = 1; temp != NULL && count != index; count++)
 	{
 		temp = temp->next;
 	}
