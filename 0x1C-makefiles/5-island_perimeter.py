d7aa6087bcdd33e121e3688ca2c5122f9432d02d
root <root@J28-SP7.localdomain>
2022-03-17 19:56:20 -0400

    Modified file task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,8 +3,6 @@
 Module Island perimeter
 """
 
-import operator
-
 
 class Solution(object):
     def islandPerimeter(self, grid):
@@ -29,4 +27,4 @@ class Solution(object):
 # (more precisely, the edge between them is). So just count the differing pairs, both horizontally and vertically
 # (for the latter I simply transpose the grid).
     def islandPerimeter2(self, grid):
-        return sum(sum(map(operator.ne, [0] + row, row + [0])) for row in grid + map(list, zip(*grid)))
+        return sum(sum(map([0] + row, row + [0])) for row in grid + map(list, zip(*grid)))
